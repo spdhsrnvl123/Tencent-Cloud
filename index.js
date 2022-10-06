c3eb94c9d566183ce9548961cd0563f3e19c0876
이태형 <spdhsrnvl123@naver.com>
2022-10-06 13:48:18 +0900

    🎉햄버거 메뉴 버튼 기능구현

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//슬라이드 쇼
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
@@ -6,4 +7,15 @@ left.addEventListener('click', function() {
 })
 right.addEventListener('click', function() {
     document.querySelector(".content_2").style.transform = "translate(-638px)";
+})
+
+//햄버거 메뉴 토글 버튼
+
+const hambuger = document.querySelector(".hambuger");
+const nav = document.querySelector(".nav")
+const header = document.querySelector(".header")
+
+hambuger.addEventListener("click",()=>{
+    nav.classList.toggle("active");
+    header.classList.toggle("active");
 })
