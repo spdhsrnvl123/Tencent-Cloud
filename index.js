100feea86322d22bb066470045a4048d4676b0c4
이태형 <spdhsrnvl456@gmail.com>
2023-01-22 23:37:43 +0900

    ♻︎refactor:코드 리펙토링

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ right.addEventListener('click', function() {
     document.querySelector(".content_2").style.transform = "translate(-638px)";
 })
 
-//햄버거 메뉴 토글 버튼
-
 const hambuger = document.querySelector(".hambuger");
 const nav = document.querySelector(".nav")
 const header = document.querySelector(".header")
